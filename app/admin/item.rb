f80af1b177229d9dfe17c88880ec0442f60f4545
StasLakh <doctorbyron05@gmail.com>
2017-07-11 05:56:18 -0700

    fix paperclip in activeadmin

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -1,9 +1,38 @@
 ActiveAdmin.register Item do
 
-  permit_params :name,
-              :description,
-              :oldprice,
-              :newprice,
-              :image
 
+  permit_params :name, :description, :oldprice, :newprice, :image
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :oldprice
+      f.input :newprice
+      f.input :image, :required => false, :as => :file
+    end
+    f.submit
+  end
+
+  index do
+    column :name
+    column :description
+    column :oldprice
+    column :newprice
+
+    actions
+  end
+
+  show do |ad|
+    attributes_table do
+      row :name
+      row :description
+      row :oldprice
+      row :newprice
+      row :image do
+        image_tag(ad.image.url(:thumb))
+      end
+      # Will display the image on show object page
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :items
+
 
 end
